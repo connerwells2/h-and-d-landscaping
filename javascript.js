cf5457b74569d5e9ab1115c7f2ecd55f1755c12b
connerwells2 <64913478+connerwells2@users.noreply.github.com>
2020-07-17 09:01:19 -0400

    Added form validation, media queries, etc.

---
+++ javascript.js
@@ -0,0 +1,8 @@
+function validateEmail() {
+    var x = document.forms['contactus.html'] ['email'].value;
+    if (x == "a@b.com") {
+        alert("Email must be properly formatted");
+        return false;
+    }
+}
+
